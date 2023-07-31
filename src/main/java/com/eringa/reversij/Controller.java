5d21a4f001a53832d925ccdda083394d47ccf9fd
DirkJanE <65190284+DirkJanE@users.noreply.github.com>
2023-07-31 16:26:51 +0200

    First upload.

---
+++ src/main/java/com/eringa/reversij/Controller.java
@@ -0,0 +1,69 @@
+package com.eringa.reversij;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static javafx.scene.shape.StrokeType.INSIDE;
+
+public class Controller implements Initializable {
+
+    @FXML public GridPane grid;
+    @FXML public Text text, text1;
+    @FXML public Circle sourcecircle;
+
+    private String score;
+
+    Circle[] circles = new Circle[64];
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        score = "1";
+        text.setText(score);
+
+        for (int count = 0; count < 64; count++) {
+            Circle circle = new Circle();
+            circle.setId("circle" + count);
+            circle.setFill(Color.LIGHTBLUE);
+            circle.setStroke(Color.BLACK);
+            circle.setStrokeType(INSIDE);
+            circle.setRadius(23);
+            circle.setOnMouseClicked(e -> handleMouseClick(e));
+
+            if (count == 28 || count == 35) {
+                circle.setFill(Color.RED);
+            } else if (count == 36 || count == 27) {
+                circle.setFill(Color.BLACK);
+            }
+            circles[count] = circle;
+        }
+
+        int cell  = 0;
+        for (int row = 0; row < 8; row++) {
+            for (int column = 0; column < 8; column++) {
+                    grid.add(circles[cell], column, row);
+                    cell++;
+                }
+        }
+    }
+
+    public void setTextArea() {
+        System.out.println("Text Clicked!");
+        text.setText("2");
+        text1.setText("2");
+    }
+
+    public void handleMouseClick(MouseEvent e) {
+        System.out.println("Circle clicked!");
+        sourcecircle = (Circle) e.getSource();
+        System.out.println(sourcecircle.getId());
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.eringa.reversij {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.eringa.reversij to javafx.fxml;
+    exports com.eringa.reversij;
+}
