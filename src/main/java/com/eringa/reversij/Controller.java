70ca0e4768407013ded0f4466dfad240c6dc59c2
DirkJanE <65190284+DirkJanE@users.noreply.github.com>
2023-08-21 15:27:35 +0200

    Updated various functions.

--- src/main/java/com/eringa/reversij/Controller.java
+++ src/main/java/com/eringa/reversij/Controller.java
@@ -9,29 +9,34 @@ import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-import static com.eringa.reversij.GameLogic.checkLine;
+import static com.eringa.reversij.GameLogic.*;
 import static javafx.scene.shape.StrokeType.INSIDE;
 
 public class Controller implements Initializable {
 
     @FXML public GridPane grid;
-    @FXML public Text text, text1;
+    @FXML public Text text1, text2, playerturn;
     @FXML public Circle sourcecircle;
 
     private String score;
 
-    Circle[] circles = new Circle[64];
+    String hexcolorred = "0xff0000ff";
+    String hexcolorblack = "0x000000ff";
 
-    Boolean validline;
+    Circle[] circles = new Circle[208];
+    ArrayList<Integer> changeids = new ArrayList<>();
+
+    Boolean player1move = true;
+    String playercolor;
+    String opponentcolor;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        score = "1";
-        text.setText(score);
 
-        for (int count = 0; count < 64; count++) {
+        for (int count = 0; count < 208; count++) {
             Circle circle = new Circle();
             circle.setId("circle" + count);
             circle.setFill(Color.LIGHTBLUE);
@@ -40,33 +45,62 @@ public class Controller implements Initializable {
             circle.setRadius(23);
             circle.setOnMouseClicked(e -> handleMouseClick(e));
 
-            if (count == 28 || count == 35) {
+            if (count == 100 || count == 107) {
                 circle.setFill(Color.RED);
-            } else if (count == 36 || count == 27) {
+            } else if (count == 108 || count == 99) {
                 circle.setFill(Color.BLACK);
             }
             circles[count] = circle;
         }
 
-        int cell  = 0;
+        int cell = 72;
         for (int row = 0; row < 8; row++) {
             for (int column = 0; column < 8; column++) {
-                    grid.add(circles[cell], column, row);
-                    cell++;
-                }
+                grid.add(circles[cell], column, row);
+                cell++;
+            }
         }
-    }
 
-    public void setTextArea() {
-        System.out.println("Text Clicked!");
-        text.setText("2");
-        text1.setText("2");
+    playerturn.setText("It's your turn, player 1!");
+    updateScore(circles, text1, text2, hexcolorred, hexcolorblack);
+
     }
 
     public void handleMouseClick(MouseEvent e) {
-        System.out.println("Circle clicked!");
+
         sourcecircle = (Circle) e.getSource();
         String circleid = sourcecircle.getId();
-        System.out.println(checkLine(circleid, circles));
+
+        if (player1move == true) {
+
+            playercolor = hexcolorred;
+            opponentcolor = hexcolorblack;
+            checkLine(circleid, circles, changeids, playercolor, opponentcolor);
+            if (changeids.size() > 0) {
+                updateBoard(circles, changeids, playercolor);
+                playerturn.setText("It's your turn, player 2!");
+                player1move = false;
+            }
+
+        } else {
+
+            playercolor = hexcolorblack;
+            opponentcolor = hexcolorred;
+            checkLine(circleid, circles, changeids, playercolor, opponentcolor);
+            if (changeids.size() > 0) {
+                updateBoard(circles, changeids, playercolor);
+                playerturn.setText("It's your turn, player 1!");
+                player1move = true;
+            }
+        }
+
+        /*
+        for (int i = 0; i < changeids.size(); i++) {
+            System.out.println(changeids.get(i));
+        }
+        */
+        changeids.clear();
+
+        updateScore(circles, text1, text2, hexcolorred, hexcolorblack);
     }
 }
--- src/main/java/com/eringa/reversij/GameLogic.java
+++ src/main/java/com/eringa/reversij/GameLogic.java
@@ -1,31 +1,174 @@
 package com.eringa.reversij;
 
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class GameLogic {
 
-    public static Boolean checkLine(String circleid, Circle[] circles) {
-        //System.out.print(circleid);
+    //static List<Integer> offsets = new ArrayList<>(List.of(-9, -8, -7, -1, 1, 7, 8, 9));
+
+    public static void updateScore(Circle[] circles, Text text1, Text text2, String color1, String color2) {
+
+        int score1 = 0;
+        int score2 = 0;
+        for (int i = 72; i < 136; i++) {
+            if (circles[i].getFill().toString().equals(color1)) {
+                score1++;
+                text1.setText(String.valueOf(score1));
+            } else if (circles[i].getFill().toString().equals(color2)) {
+                score2++;
+                text2.setText(String.valueOf(score2));
+            }
+        }
+        //System.out.println(score);
+    }
+
+    public static ArrayList<Integer> checkLine(String circleid, Circle[] circles, ArrayList<Integer> changeids, String playercolor, String opponentcolor) {
 
         Integer id;
+        ArrayList<Integer> tempids = new ArrayList<>();
 
         if (circleid.length() == 8) {
             id = Integer.valueOf(circleid.substring(6, 8));
         }
         else {
-            id = Integer.valueOf(circleid.substring(6, 7));
+            id = Integer.valueOf(circleid.substring(6, 9));
         }
 
-        String hexcolor = "0xadd8e6ff";
+        //System.out.println("id: " + id);
 
-        for (int count = 1; count < 4; count++) {
-            if (circles[id - count].getFill().toString().equals(hexcolor)) {
+        String hexcolorblue = "0xadd8e6ff";
 
-            } else {
-                return false;
+        List<Integer> offsets = new ArrayList<>();
+
+        Integer offset = 0;
+        Integer[] boardlimits = {72, 73, 74, 75, 76, 77, 78, 79, 80, 88, 96, 104, 112, 120, 87, 95, 103, 111, 119, 127, 128, 129, 130,
+        131, 132, 133, 134, 135};
+
+        offsets = getOffsets(id, boardlimits, offsets);
+
+        /*
+
+        for (int q = 0; q < offsets.size(); q++) {
+            System.out.println("Print: " + offsets.get(q));
+        }
+
+         */
+
+        for (int i = 0; i < offsets.size(); i++) {
+            //System.out.println(i);
+
+            if (circles[id].getFill().toString().equals(hexcolorblue)) {
+                tempids.add(id);
+                //System.out.println("Added id: " + id);
+
+                for (int j = 1; j < 8; j++) {
+
+                    offset = id + (offsets.get(i) * j);
+                    //System.out.println(offset);
+
+                    if (j == 1) {
+                        if (circles[offset].getFill().toString().equals(opponentcolor)) {
+                            System.out.println("j = 1, ID: " + id);
+                            System.out.println("j = 1, Offset: " + offsets.get(i) * j);
+                            System.out.println("j = 1, New id: " + (id + offsets.get(i) * j));
+                            tempids.add(offset);
+                            //System.out.println("j == 1, match opponent color");
+                        } else {
+                            tempids.clear();
+                            break;
+                        }
+                    } else if (j > 1) {
+                        System.out.println("j > 1, ID: " + id);
+                        System.out.println("j > 1, Offset: " + offsets.get(i) * j);
+                        System.out.println("j > 1, New id: " + (id + offsets.get(i) * j));
+
+                        if (circles[offset].getFill().toString().equals(hexcolorblue)) {
+                            System.out.println("First break");
+                            tempids.clear();
+                            break;
+                        } else if (circles[offset].getFill().toString().equals(opponentcolor)) {
+                            System.out.println("j > 1, match opponentcolor");
+
+                            Integer finalOffset = offset;
+                            if (!Arrays.stream(boardlimits).anyMatch(s -> s.equals(finalOffset))) {
+                                tempids.add(offset);
+                            } else {
+                             System.out.println("Second break");
+                             tempids.clear();
+                             break;
+                            }
+
+                        } else if (circles[offset].getFill().toString().equals(playercolor)) {
+                            //System.out.println("j > 1, match player color");
+                            for (int k = 0; k < tempids.size(); k++) {
+                                //System.out.println(tempids.get(k));
+                                changeids.add(tempids.get(k));
+                            }
+                            break;
+                        }
+                    }
+                }
             }
         }
-        return true;
+
+        tempids.clear();
+
+        return changeids;
+    }
+
+    public static void updateBoard(Circle[] circles, ArrayList<Integer> changeids, String playercolor) {
+
+        for (int count = 0; count < changeids.size(); count++) {
+            for (int secondcount = 0; secondcount < circles.length; secondcount++) {
+                if (changeids.get(count) == secondcount) {
+                    circles[secondcount].setFill(Paint.valueOf(playercolor));
+                }
+
+            }
+        }
+        changeids.clear();
+    }
+
+    public static List<Integer> getOffsets(Integer id, Integer[] boardlimits, List<Integer> offsets) {
+        Integer[] topleftlimit = Arrays.copyOfRange(boardlimits, 0, 1);
+        Integer[] toprightlimit = Arrays.copyOfRange(boardlimits, 7, 8);
+        Integer[] bottomleftlimit = Arrays.copyOfRange(boardlimits, 20, 21);
+        Integer[] bottomrightlimit = Arrays.copyOfRange(boardlimits, 27, 28);
+        Integer[] toplimits = Arrays.copyOfRange(boardlimits, 1, 7);
+        Integer[] leftlimits = Arrays.copyOfRange(boardlimits, 8, 14);
+        Integer[] rightlimits = Arrays.copyOfRange(boardlimits, 14, 20);
+        Integer[] bottomlimits = Arrays.copyOfRange(boardlimits, 21, 27);
+
+        if (Arrays.stream(boardlimits).anyMatch(s -> s.equals(id))) {
+            if (Arrays.stream(topleftlimit).anyMatch(s -> s.equals(id))) {
+                offsets = List.of(1, 8, 9);
+            } else if (Arrays.stream(toprightlimit).anyMatch(s -> s.equals(id))) {
+                offsets = List.of(-1, 8, 7);
+            } else if (Arrays.stream(bottomleftlimit).anyMatch(s -> s.equals(id))) {
+                offsets = List.of(1, -8, -7);
+            } else if (Arrays.stream(bottomrightlimit).anyMatch(s -> s.equals(id))) {
+                offsets = List.of(-1, -8, -9);
+            } else if (Arrays.stream(toplimits).anyMatch(s -> s.equals(id))) {
+                offsets = List.of(-1, 1, 7, 8, 9);
+            } else if (Arrays.stream(leftlimits).anyMatch(s -> s.equals(id))) {
+                offsets = List.of(-8, -7, 1, 8, 9);
+            } else if (Arrays.stream(rightlimits).anyMatch(s -> s.equals(id))) {
+                offsets = List.of(-9, -8, -1, 7, 8);
+            } else if (Arrays.stream(bottomlimits).anyMatch(s -> s.equals(id))) {
+                offsets = List.of(-9, -8, -7, -1, 1);
+            }
+        } else {
+            offsets = List.of(-9, -8, -7, -1, 1, 7, 8, 9);
+        }
+        return offsets;
     }
 
 }
